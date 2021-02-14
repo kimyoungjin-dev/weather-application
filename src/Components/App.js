d63ca54dd9a0f1d71a33039639557154d2f342f5
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-15 04:17:41 +0900

    Import globalstyled bg.js

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import GlobalStyles from "Components/GlobalStyles";
+import Bg from "Components/Bg";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
