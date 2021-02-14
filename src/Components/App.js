f3e0c5ec970ec1a61b80b5026f449b37e0fef43e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-15 04:59:14 +0900

    Import and receive arguments from the component.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import GlobalStyles from "Components/GlobalStyles";
+import Router from "Components/Router";
 import Bg from "Components/Bg";
-import WetherIcons from "Components/WetherIcons";
 import DateCollection from "Components/DateCollection";
+import WetherIcons from "Components/WetherIcons";
 
 const App = () => {
-  return <div></div>;
+  const DateArray = DateCollection();
+  const weatherIcon = WetherIcons();
+
+  return (
+    <div>
+      <GlobalStyles />
+      <Bg />
+      <Router DateArray={DateArray} weatherIcon={weatherIcon} />
+    </div>
+  );
 };
 
 export default App;
