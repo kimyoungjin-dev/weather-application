8cbc60e5a0cfbf1930dd57bd2c8c9b1603b83372
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-15 04:22:42 +0900

    Import WetherIconsr and DateCollection

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import GlobalStyles from "Components/GlobalStyles";
 import Bg from "Components/Bg";
+import WetherIcons from "Components/WetherIcons";
+import DateCollection from "Components/DateCollection";
 
 const App = () => {
   return <div></div>;
