5d3eea51ef791c20b535cf175aaa43d1d887eb29
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-15 04:21:59 +0900

    Make a component of the status bar visible to all routers

---
+++ src/Components/Status.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { BsBatteryHalf, BsFilterRight } from "react-icons/bs";
+import { BiWifi } from "react-icons/bi";
+
+const Container = styled.div`
+  padding: 10px; /////padding 10px
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+`;
+
+const Time = styled.div``;
+
+const ItemList = styled.div``;
+
+const Item = styled.span`
+  margin-left: 10px;
+`;
+
+const Status = ({}) => {
+  const [clock, setClock] = useState("");
+
+  const getClock = () => {
+    const date = new Date();
+    const getHours = date.getHours();
+    const Minutes = date.getMinutes();
+    setClock(
+      `${getHours < 10 ? `0${getHours}` : getHours}:${
+        Minutes < 10 ? `0${Minutes}` : Minutes
+      }`
+    );
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      getClock();
+    }, 1000);
+  }, []);
+
+  return (
+    <Container>
+      <Time>{clock}</Time>
+
+      <ItemList>
+        <Item>
+          <BsBatteryHalf />
+        </Item>
+
+        <Item>
+          <BiWifi />
+        </Item>
+
+        <Item>
+          <BsFilterRight />
+        </Item>
+      </ItemList>
+    </Container>
+  );
+};
+
+export default Status;
