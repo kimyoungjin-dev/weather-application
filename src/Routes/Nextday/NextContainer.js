b442adcbc1876e1ade253adbda6e877a13610fc2
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-15 04:34:05 +0900

    Request api containing 7 days of future forecast information, and pass arguments to files in each next folder.

--- src/Routes/Nextday/NextContainer.js
+++ src/Routes/Nextday/NextContainer.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { futureApi } from "api";
+import NextdayPresenter from "./NextdayPresenter";
+import NextForm from "./NextForm";
+import NextDaily from "./NextDaily";
 
-const NextContainer = () => {
-  return <div></div>;
+const NextContainer = ({ weatherIcon, DateArray }) => {
+  const [NextData, setNextData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getWeather = async (lat, lon) => {
+    try {
+      const {
+        data: { daily: NextData },
+      } = await futureApi.getFuture(lat, lon);
+      setNextData(NextData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        getWeather(lat, lon);
+      },
+      () => console.log("Can find you :(")
+    );
+  }, []);
+
+  return (
+    <div>
+      <NextdayPresenter loading={loading} />
+      <NextForm
+        DateArray={DateArray}
+        weatherIcon={weatherIcon}
+        NextData={NextData}
+        loading={loading}
+      />
+      <NextDaily
+        DateArray={DateArray}
+        weatherIcon={weatherIcon}
+        NextData={NextData}
+        loading={loading}
+      />
+    </div>
+  );
 };
 
 export default NextContainer;
