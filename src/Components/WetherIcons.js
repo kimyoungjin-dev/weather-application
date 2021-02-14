e734f75f6d31a0fdc092444daf8575eb7e4396af
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-02-15 04:22:16 +0900

    Create a component that changes weather-related information into icons

---
+++ src/Components/WetherIcons.js
@@ -0,0 +1,29 @@
+import React from "react";
+//icons
+import { AiOutlineAntCloud } from "react-icons/ai";
+import { IoThunderstormOutline } from "react-icons/io5";
+import { RiDrizzleFill } from "react-icons/ri";
+import {
+  FaSnowflake,
+  FaSmog,
+  FaCloudMoonRain,
+  FaCloudSun,
+} from "react-icons/fa";
+import { IoPartlySunnyOutline } from "react-icons/io5";
+import { WiDayCloudyWindy } from "react-icons/wi";
+
+export default () => {
+  const weatherIcon = {
+    Clouds: <AiOutlineAntCloud />,
+    Drizzle: <RiDrizzleFill />,
+    Thunderstorm: <IoThunderstormOutline />,
+    Rain: <FaCloudMoonRain />,
+    Snow: <FaSnowflake />,
+    Clear: <FaCloudSun />,
+    Haze: <IoPartlySunnyOutline />,
+    Mist: <FaSmog />,
+    Dust: <WiDayCloudyWindy />,
+  };
+
+  return weatherIcon;
+};
