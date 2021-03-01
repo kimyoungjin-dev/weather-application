522fa03074e417851f374dacdd894b5c5dca2926
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 02:32:56 +0900

    Simplify the code

--- src/Components/Bg.js
+++ src/Components/Bg.js
@@ -2,31 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import bgImage from "Images/backgroudeImage.gif";
 
-const Container = styled.div`
-  position: fixed;
-  height: 100px;
-  width: 100px;
-  z-index: -1;
-`;
-
-const ImageContainer = styled.div`
-  height: 100vh;
-  width: 100vw;
-`;
-
 const Image = styled.img`
   height: 100vh;
   width: 100vw;
 `;
 
-const Bg = () => {
-  return (
-    <Container>
-      <ImageContainer>
-        <Image src={bgImage} />
-      </ImageContainer>
-    </Container>
-  );
-};
+const Bg = () => <Image src={bgImage} />;
 
 export default Bg;
