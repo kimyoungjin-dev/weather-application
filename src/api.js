a45aca72415f6dacf0919bd9b964f7129524ee9a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 02:34:06 +0900

    Modify the api.js code

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
-import React from "react";
+import axios from "axios";
 
-const api = () => {
-  return <div></div>;
+const makeRequest = (path, params) =>
+  axios.get(`https://api.openweathermap.org/data/2.5/`, {
+    params: {
+      ...params,
+      appid: "1cce20274897538f27f72a370b9bb045",
+    },
+  });
+
+export const current = {
+  getCurrent: (lat, lon) => makeRequest("onecall?", { lat, lon }),
 };
-
-export default api;
