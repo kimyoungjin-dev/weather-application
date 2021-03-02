140da06ce83189b45a8aca15217c2bc8f15fad2a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 17:12:06 +0900

    Clean up the loading screen code and change the image

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,60 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { FaSpinner } from "react-icons/fa";
-import LoadingImage from "Images/backgroudeImage.gif";
+import bgImage from "../Images/Loading.gif";
 
-const Container = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
+const Image = styled.img`
   width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-  color: black;
-  z-index: 1;
+  height: 100%;
 `;
 
-const Loading = styled.img`
-  position: fixed;
-`;
+const Loading = () => <Image src={bgImage} />;
 
-const SpinContainer = styled.div`
-  width: 50%;
-  height: 30vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Spin = styled.span`
-  font-size: 60px;
-  animation: spin 4s linear infinite;
-  @keyframes spin {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-`;
-
-const Loader = () => {
-  return (
-    <Container>
-      <Loading src={LoadingImage}></Loading>
-      <SpinContainer>
-        <Spin>
-          <FaSpinner />
-        </Spin>
-      </SpinContainer>
-    </Container>
-  );
-};
-
-export default Loader;
+export default Loading;
