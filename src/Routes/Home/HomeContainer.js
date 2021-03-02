83646d4ef237af71f305e1479b98e93978f385b4
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 17:14:11 +0900

    Create a container to bring api information and location information.

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { current } from "api";
+import { useDispatch, useState } from "./Context";
+import { SUCCESS } from "./Reducer";
+import Form from "./Form";
+import Navigation from "Components/Navigation";
+import Bottom from "./Bottom";
+import Loading from "Components/Loading";
 
 const HomeContainer = () => {
-  return <div></div>;
+  const { dispatch } = useDispatch();
+  const { loading } = useState();
+
+  const getWeather = async (lat, lon) => {
+    const [data, dataError] = await current.getCurrent(lat, lon);
+    dispatch({ type: SUCCESS, payload: data, dataError });
+  };
+
+  const getLocation = () => {
+    const handleSuccess = (position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      getWeather(lat, lon);
+    };
+    const handleError = () => {
+      console.log("위치정보를 찾을수없습니다.");
+    };
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Form />
+          <Navigation />
+          <Bottom />
+        </>
+      )}
+    </>
+  );
 };
 
 export default HomeContainer;
