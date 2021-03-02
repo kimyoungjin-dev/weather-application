6a68917320e391a6bb751d6996792efbcaddfc9f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 17:12:38 +0900

    Change the location of bg component and router component in app.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Globalstyles from "Components/Globalstyles";
 import Router from "Components/Router";
-import Bg from "Components/Bg";
 import Status from "./Status";
 
 const App = () => {
   return (
     <>
-      <Router />
       <Globalstyles />
       <Status />
-      <Bg />
+      <Router />
     </>
   );
 };
