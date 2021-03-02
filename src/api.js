aafb4c80ff9f31452aeb40e085aa4dc2c8a64758
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 17:12:57 +0900

    Create an api component to receive weather information

--- src/api.js
+++ src/api.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
 const makeRequest = (path, params) =>
-  axios.get(`https://api.openweathermap.org/data/2.5/`, {
+  axios.get(`https://api.openweathermap.org/data/2.5/${path}`, {
     params: {
       ...params,
       appid: "1cce20274897538f27f72a370b9bb045",
     },
   });
 
+const CurrentHourly = async (path, params = {}) => {
+  try {
+    const { data } = await makeRequest(path, params);
+    return [data, null];
+  } catch (error) {
+    console.log(error);
+    return [error, null];
+  }
+};
+
 export const current = {
-  getCurrent: (lat, lon) => makeRequest("onecall?", { lat, lon }),
+  getCurrent: (lat, lon) => CurrentHourly("onecall", { lat, lon }),
 };
