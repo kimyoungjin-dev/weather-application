0bf55fe5ab58ece48f797423290ecf0c230489f7
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-02 20:49:57 +0900

    Delete yesterday.js router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 import Home from "Routes/Home";
 import Nextday from "Routes/Nextday";
-import Yesterday from "Routes/Yesterday";
 
 export default () => {
   return (
@@ -14,7 +13,6 @@ export default () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/nextday" component={Nextday} />
-        <Route exact path="/yesterday" component={Yesterday} />
         <Redirect from="*" to="/" />
       </Switch>
     </Router>
